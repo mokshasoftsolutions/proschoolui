2f1e947d7819640d1d3ca14803976822566393ba
narasimha <mokshasoftsolutions@gmail.com>
2017-10-05 12:11:34 +0530

    employee

--- app/components/attendenceListEmployee/attendanceListEmployeeController.js
+++ app/components/attendenceListEmployee/attendanceListEmployeeController.js
@@ -1,96 +1,18 @@
 angular.module('school_erp')
-    .controller("attendanceListEmployeeController", ['$http', '$scope', '$rootScope', '$window', 'globalServices', 'examServices', 'subjectsServices', 'studentServices','employeeServices','donutChartOneService', 'ngDialog', function ($http, $scope, $rootScope, $window, globalServices, examServices, subjectsServices, studentServices,employeeServices, donutChartOneService, ngDialog) {
+    .controller("attendanceListEmployeeController", ['$http', '$scope', '$rootScope', '$window', 'globalServices', 'examServices', 'subjectsServices', 'studentServices', 'employeeServices', 'donutChartOneService', 'ngDialog', function ($http, $scope, $rootScope, $window, globalServices, examServices, subjectsServices, studentServices, employeeServices, donutChartOneService, ngDialog) {
 
-        var d = new Date();
-        var n = d.getMonth();
 
-        $scope.select_month = n;
-        $scope.months = [{
-            name: "January",
-            id: 1
-        },
-        {
-            name: "February",
-            id: 2
-        },
-        {
-            name: "March",
-            id: 3
-        },
-        {
-            name: "April",
-            id: 4
-        },
-        {
-            name: "May",
-            id: 5
-        },
-        {
-            name: "June",
-            id: 6
-        },
-        {
-            name: "July",
-            id: 7
-        },
-        {
-            name: "August",
-            id: 8
-        },
-        {
-            name: "September",
-            id: 9
-        },
-        {
-            name: "October",
-            id: 10
-        },
-        {
-            name: "November",
-            id: 11
-        }, {
-            name: "December",
-            id: 12
-        }
-        ]
 
-        $scope.evalData = [];
-        $scope.data = [];
         $scope.select_date = new Date().toDateString();
-        //   $scope.getAttendenceByDay($scope.select_date);
+
         $scope.initialLoadAttendence = false;
 
-        // $scope.getClassesInitalLoad = function () {
-        //     globalServices.getClass()
-        //         .success(function (data, status) {
-        //             $scope.classDatanew = data.school_classes; // Api list-name
-        //             $scope.classId = data.school_classes[0].class_id;
-
-        //             $scope.populateSections($scope.classId);
-        //         })
-        //         .error(function (data, success) { })
-        // }
-        // $scope.populateSections = function (classId) {
-        //     globalServices.getSections(classId)
-        //         .success(function (data, status) {
-        //             $scope.secData = data.class_sections; // Api list-name
-        //             $scope.secId = data.class_sections[0].section_id;
-        //             $scope.getStudentValue($scope.secId);
-        //             if ($scope.initialLoadAttendence == false) {
-        //                 $scope.getAttendenceByDay($scope.select_date, $scope.classId, $scope.secId);
-        //             }
-
-        //             // $scope.getAttendence($scope.classId,$scope.secId);
-
-        //         })
-        //         .error(function (data, success) { })
-        // }
 
         $scope.getAttendenceByDay = function (date) {
+            console.log("message");
+            console.log(date);
             $scope.initialLoadAttendence = true;
-            // var arrPresent = new Array();
-            // var arrAbsent = new Array();
-            // var arrLeave = new Array();
+
             $scope.attData = [];
 
             employeeServices.getEmployeeAttendenceByDay(date)
@@ -103,51 +25,10 @@ angular.module('school_erp')
 
 
 
-        // $scope.getStudentValue = function (secValue) {
-        //     studentServices.getStudent(secValue)
-        //         .success(function (data, status) {
-        //             $scope.students = data.students;
-        //             $scope.studentId = data.students[0].student_id;
-        //             console.log($scope.studentId);
-        //         })
-        //         .error(function (data, success) { })
+
+
+        // $scope.showRole = function (role) {
+        //     return globalServices.fetchRoleAuth(role);
         // }
 
-        // Role based Display
-
-        // $scope.getMonth = function (select_month) {
-        //     $scope.monthId = $scope.select_month;
-        //     console.log($scope.monthId);
-        //     // $scope.getAttendenceByMonth($scope.monthId, $scope.studentId);
-
-        // }
-
-        //  $scope.getAttendenceByMonth = function (month, studentId) {
-        //     // var arrPresent = new Array();
-        //     // var arrAbsent = new Array();
-        //     // var arrLeave = new Array();
-        //     $scope.attDataMonth = [];
-        //     studentServices.getAttendenceByMonth(month, studentId)
-        //         .success(function (data, status) {
-        //             $scope.attDataMonth = data.donutchart;
-        //             console.log(JSON.stringify(data));
-        //             })
-        //         .error(function (data, success) { })
-        // }
-
-        // Role based Display
-        $scope.showRole = function (role) {
-            return globalServices.fetchRoleAuth(role);
-        }
-
-        // if ($rootScope.role == 'parent') {
-
-        //     $scope.secId = $rootScope.student.section;
-        //     $scope.getStudentValue($scope.secId);
-
-
-        // } else {
-        //     $scope.getClassesInitalLoad();
-        // }
-     
     }])
